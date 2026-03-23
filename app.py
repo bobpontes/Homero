6afe9fab2c28a8cbc029f141247186ade5efb6e2
Bruno Pontes <pontesbruno.eng@gmail.com>
2026-03-23 11:25:38 -0300

    feat(contas-bancarias): adiciona criação e listagem de contas bancárias

--- app.py
+++ app.py
@@ -906,6 +906,60 @@ def remover_grupo_receita(grupo_id):
 
     return redirect(url_for("contas_receber"))
 
+@app.route("/contas_bancarias")
+def contas_bancarias():
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM contas_bancarias ORDER BY saldo DESC")
+    contas_banco = cursor.fetchall()
+
+    cursor.execute("SELECT SUM(saldo) FROM contas_bancarias")
+    resultado = cursor.fetchone()
+    saldo_total_contas = resultado[0] if resultado and resultado[0] else 0
+
+    conn.close()
+
+    return render_template("contas_bancarias.html", contas_banco=contas_banco, saldo_total_contas=saldo_total_contas)
+
+@app.route("/contas_bancarias/nova", methods=["POST"])
+def nova_conta_bancaria():
+
+    nome = request.form.get("nome")
+    tipo = request.form.get("tipo") or "outro"
+    saldo = request.form.get("saldo")
+    ativo = 1 if request.form.get("ativo") else 0
+
+    if not nome:
+        abort(400, "Nome da conta é obrigatório.")
+
+    try:
+        saldo = float(saldo)
+    except (TypeError, ValueError):
+        abort(400, "Valor inválido para saldo inicial.")
+
+    conn = get_db()
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT nome FROM contas_bancarias WHERE nome = ?", (nome, ))
+    conta_existente = cursor.fetchone()
+
+    if conta_existente:
+        conn.close()
+        abort(400, "Esta conta já foi registrada em nosso sistema.")    
+    
+    cursor.execute(
+        "INSERT INTO contas_bancarias (nome, tipo, saldo, ativo) VALUES (?, ?, ?, ?)",
+        (nome, tipo, saldo, ativo)
+    )
+    
+    conn.commit()
+    conn.close()
+
+    return redirect(url_for("contas_bancarias"))
+    
+
 def criar_banco():
     conn = get_db()
     cursor = conn.cursor()
@@ -916,6 +970,16 @@ def criar_banco():
             nome TEXT NOT NULL,
             idade INTEGER NOT NULL,
             turma TEXT NOT NULL
+        )
+    ''')
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS contas_bancarias (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            nome TEXT NOT NULL,
+            tipo TEXT,
+            saldo REAL DEFAULT 0,
+            ativo BOOLEAN DEFAULT 1
         )
     ''')
 
